3e88d52e77782992815bbb84be8f6131b2f0223f
Gurkirat Singh <tablagurkirat@yahoo.com>
2016-11-20 04:29:56 -0800

    brute fetcher

---
+++ BruteDownloader.py
@@ -0,0 +1,90 @@
+from Queue import Queue
+import threading
+import subprocess,re,fcntl,sys
+
+class ThreadWorker(threading.Thread):
+	def __init__(self,queue):
+		threading.Thread.__init__(self)
+		self.queue = queue
+
+	def run(self):
+		while self.queue.qsize() != 0:
+			download, args = self.queue.get() 
+			download(args,self.getRTMPAddress(args[1],args[0]))
+			self.queue.task_done()
+
+	def getRTMPAddress(self,user,rec_id):
+		return "{0}{1}/{2}{3}".format("rtmp://streams2.webconferencingonline.com/",user,
+			rec_id, "/FCAVPresence.vidConf1_mc.av.1")
+
+class BruteValidator:
+	def __init__(self):
+		self.queue = Queue()
+		self.arg_list = ["rtmpdump","-o", "/dev/null","--stop", "0.01","-r"]
+		self.month_map = {
+			"Jan":1,
+			"Feb":2,
+			"Mar":3,
+			"Apr":4,
+			"May":5,
+			"Jun":6,
+			"Jul":7,
+			"Aug":8,
+			"Sep":9,
+			"Oct":10,
+			"Nov":11,
+			"Dec":12
+		}
+		self.numthreads = 8
+		self.outfile = open("brute_nums.txt","a+")
+		self.outfile.write("\n")
+		self.outfile.flush()
+		print "File opened"
+		self.lock = threading.Lock()
+		self.queueUp()
+		self.launchThreads()
+		self.queue.join()
+		self.outfile.close()
+		print "File closed"
+
+	def writeToFile(self,filename,rec_id,user):
+		self.lock.acquire()
+		self.outfile.write("{0} {1} {2}\n".format(rec_id,user,filename))
+		self.outfile.flush()
+		self.lock.release()
+
+	def download(self,args,address):
+		out = None
+		try:
+			out = subprocess.check_output(self.arg_list+ [address],stderr= subprocess.STDOUT)
+		except subprocess.CalledProcessError,e:
+			out = e.output
+		finally:
+			date_m = re.search(r"creationdate\s*(.*)",out)
+			if date_m:
+				date_list = date_m.group(1).split()
+				month = str(self.month_map[date_list[1].strip()])
+				date = date_list[2].strip()
+				date_sub_m = re.sub("[0]+","",date)
+				year = date_list[-1].strip()
+				filename = "{0}-{1}-{2}.flv".format(month,date,year)
+				self.writeToFile(filename,args[0],args[1])
+				sys.stdout.write("FOUND: {0} {1} {2}\n".format(args[0],args[1],filename))
+				sys.stdout.flush()
+			else:
+				sys.stdout.write("invalid: " + args[0] + "\n")
+				sys.stdout.flush()
+
+	def launchThreads(self):
+		#create 10 threads
+		for i in range(self.numthreads):
+			t_worker = ThreadWorker(self.queue)
+			t_worker.daemon = True
+			t_worker.start()
+
+	def queueUp(self):
+		for i in xrange(300000,700000):
+			for user in ["aacm4","aacm21"]:
+				self.queue.put((self.download,(str(i),user)))
+
+brute = BruteValidator()
