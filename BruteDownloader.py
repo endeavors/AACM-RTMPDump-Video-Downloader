f6c4825fccf6b8b22ff22322c90274772daafc9e
Gurkirat Singh <tablagurkirat@yahoo.com>
2016-11-20 04:44:57 -0800

    fetcher added

--- BruteDownloader.py
+++ BruteDownloader.py
@@ -1,90 +1,63 @@
-from Queue import Queue
-import threading
-import subprocess,re,fcntl,sys
+from bs4 import BeautifulSoup
+from queue import Queue
+from threading import Thread
+import subprocess,os,sys,re,json,glob,shutil
 
-class ThreadWorker(threading.Thread):
+
+class ThreadWorker(Thread):
 	def __init__(self,queue):
-		threading.Thread.__init__(self)
+		Thread.__init__(self)
 		self.queue = queue
 
 	def run(self):
 		while self.queue.qsize() != 0:
-			download, args = self.queue.get() 
-			download(args,self.getRTMPAddress(args[1],args[0]))
+			download, args_list = self.queue.get() 
+			address = getRTMPAddress(args_list[0],args_list[1])
+			download(address, args_list[2])
 			self.queue.task_done()
 
-	def getRTMPAddress(self,user,rec_id):
+	def getRTMPAddress(self,rec_id,user):
 		return "{0}{1}/{2}{3}".format("rtmp://streams2.webconferencingonline.com/",user,
 			rec_id, "/FCAVPresence.vidConf1_mc.av.1")
 
-class BruteValidator:
-	def __init__(self):
+class BruteDownloader:
+	def __init__(self,dir_path):
+		self.arg_list = ["rtmpdump", "-q"]
+		self.counter = 0
+		self.totalfiles = 0
 		self.queue = Queue()
-		self.arg_list = ["rtmpdump","-o", "/dev/null","--stop", "0.01","-r"]
-		self.month_map = {
-			"Jan":1,
-			"Feb":2,
-			"Mar":3,
-			"Apr":4,
-			"May":5,
-			"Jun":6,
-			"Jul":7,
-			"Aug":8,
-			"Sep":9,
-			"Oct":10,
-			"Nov":11,
-			"Dec":12
-		}
 		self.numthreads = 8
-		self.outfile = open("brute_nums.txt","a+")
-		self.outfile.write("\n")
-		self.outfile.flush()
-		print "File opened"
-		self.lock = threading.Lock()
+
 		self.queueUp()
 		self.launchThreads()
 		self.queue.join()
-		self.outfile.close()
-		print "File closed"
 
-	def writeToFile(self,filename,rec_id,user):
-		self.lock.acquire()
-		self.outfile.write("{0} {1} {2}\n".format(rec_id,user,filename))
-		self.outfile.flush()
-		self.lock.release()
-
-	def download(self,args,address):
-		out = None
+	def download(self,address,out_filename):
 		try:
-			out = subprocess.check_output(self.arg_list+ [address],stderr= subprocess.STDOUT)
+			subprocess.check_call(self.arg_list + ["-r", address] + ["-o", out_filename])
+			self.counter += 1
+			sys.stdout.write("Downloaded {0}  -> {1}/{2}\n".format(out_filename,self.counter,
+			self.totalfiles))
+			sys.stdout.flush()
 		except subprocess.CalledProcessError,e:
-			out = e.output
-		finally:
-			date_m = re.search(r"creationdate\s*(.*)",out)
-			if date_m:
-				date_list = date_m.group(1).split()
-				month = str(self.month_map[date_list[1].strip()])
-				date = date_list[2].strip()
-				date_sub_m = re.sub("[0]+","",date)
-				year = date_list[-1].strip()
-				filename = "{0}-{1}-{2}.flv".format(month,date,year)
-				self.writeToFile(filename,args[0],args[1])
-				sys.stdout.write("FOUND: {0} {1} {2}\n".format(args[0],args[1],filename))
-				sys.stdout.flush()
-			else:
-				sys.stdout.write("invalid: " + args[0] + "\n")
-				sys.stdout.flush()
+			sys.stdout.write("UNABLE TO DOWNLOAD: {0}\n".format(out_filename))
+			sys.stdout.flush()
 
 	def launchThreads(self):
-		#create 10 threads
 		for i in range(self.numthreads):
 			t_worker = ThreadWorker(self.queue)
 			t_worker.daemon = True
 			t_worker.start()
 
 	def queueUp(self):
-		for i in xrange(300000,700000):
-			for user in ["aacm4","aacm21"]:
-				self.queue.put((self.download,(str(i),user)))
+		with open("./brute_nums.txt","r") as in_file:
+			for line in in_file:
+				self.totalfiles += 1
+				args_list = map(lambda x: x.strip(),line.split())
+				self.queue.put((self.download,args_list))		
 
-brute = BruteValidator()
+
+
+downloader = BruteDownloader(dir_path)
+
+
